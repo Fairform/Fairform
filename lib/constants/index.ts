d60a88b4998f0f23853e86a2909618bd0516c687
Fairform <fairformcorp@gmail.com>
2025-06-22 07:45:45 +1000

    fix(constants): add NAV_LINKS export to resolve Header.tsx import error

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -5,4 +5,4 @@ export { PRICING_TIERS } from './pricing';
 export { NAV_LINKS } from './navigation';
 export { TESTIMONIALS } from './testimonials';
 export { INDUSTRIES } from './industries';
-export { INDUSTRY_REGULATIONS } from './regulations'; // Make sure this file exists
+export { INDUSTRY_REGULATIONS } from './regulations';
