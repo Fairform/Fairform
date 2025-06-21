0676a7660642b2ae12165ed391806aa5d200c5eb
Fairform <fairformcorp@gmail.com>
2025-06-22 08:09:58 +1000

    Delete lib/constants/index.ts

--- lib/constants/index.ts
+++
@@ -1,8 +0,0 @@
-// app/lib/constants/index.ts
-
-export { FEATURES } from './features';
-export { PRICING_TIERS } from './pricing';
-export { NAV_LINKS } from './navigation';
-export { TESTIMONIALS } from './testimonials';
-export { INDUSTRIES } from './industries';
-export { INDUSTRY_REGULATIONS } from './regulations';
