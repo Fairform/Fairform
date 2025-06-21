52ce3afaa8083eba75a26a11109a58fda71235b7
Fairform <fairformcorp@gmail.com>
2025-06-22 05:44:33 +1000

    Delete app/pricing/app/pricing/layout.tsx

--- app/pricing/app/pricing/layout.tsx
+++
@@ -1,9 +0,0 @@
-// app/pricing/layout.tsx
-
-export default function PricingLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen bg-[#0f0f10] text-white">
-      {children}
-    </div>
-  );
-}
