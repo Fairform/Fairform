deb0b69b7f2dd573dc7d67995df5146ed4c782bd
Fairform <fairformcorp@gmail.com>
2025-06-22 07:26:34 +1000

    fix: properly export FEATURES and update Features component

---
+++ lib/constants/index.ts
@@ -0,0 +1,8 @@
+// app/lib/constants/index.ts
+
+export * from './features';
+export * from './pricing';
+export * from './navigation';
+export * from './testimonials';
+export * from './industries';
+export * from './regulations'; // if you’ve defined INDUSTRY_REGULATIONS here
